266da3ea75e8d4cca09fdd5299739f4d5bf8087c
Utkarsh Garg <utkarsh_garg@live.com>
2019-07-29 12:48:03 +0530

    Added 'Details' component which displays the details page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Home.css';
+import ReactDOM from 'react-dom';
+import Details from '../details/Details';
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
 import moviesData from '../../assets/movieData';
@@ -73,6 +75,10 @@ class Home extends Component {
         this.setState({ artists: event.target.value });
     }
 
+    movieClickHandler = (movieId) => {
+        ReactDOM.render(<Details movieId={movieId} />, document.getElementById('root'));
+    }
+
     render() {
         const { classes } = this.props;
         return (
@@ -93,7 +99,7 @@ class Home extends Component {
                     <div className="left">
                         <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
                             {moviesData.map(movie => (
-                                <GridListTile className="released-movie-grid-item" key={"grid" + movie.id}>
+                                <GridListTile onClick={() => this.movieClickHandler(movie.id)} className="released-movie-grid-item" key={"grid" + movie.id}>
                                     <img src={movie.poster_url} className="movie-poster" alt={movie.title} />
                                     <GridListTileBar
                                         title={movie.title}
